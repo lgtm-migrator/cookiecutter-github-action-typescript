62ff73d027ece51a970074685e5ffe5286af7346
Jonas Pammer <opensource@jonaspammer.at>
2022-07-18 12:41:39 +0200

    refactor: use yarn instead of npm
    speeds up 2.69 times after first install as per local hyperfine tests made by me rn

--- {{ cookiecutter.project_slug }}/.devcontainer/postCreateCommand.sh
+++ {{ cookiecutter.project_slug }}/.devcontainer/postCreateCommand.sh
@@ -4,5 +4,5 @@ python3 -m pip install -r requirements-dev.txt
 pre-commit install --install-hooks
 #pre-commit install --hook-type commit-msg
 
-npm install -D
-npm run build
+yarn install -D
+yarn run build
